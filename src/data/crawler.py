fa629be275d9b7973b87fa263a177e5dfba2ac8c
joonwoo-yoo <dbwnsdn122@naver.com>
2026-03-03 13:14:28 +0900

    feat: implement TMDBCollector for data ingestion

---
+++ src/data/crawler.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import requests
+
+
+class TMDBCollector:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "https://api.themoviedb.org/3"
+
+    def fetch_tmdb_data(self, max_pages=5) -> pd.DataFrame:
+        all_movies = []
+
+        if not self.api_key:
+            print("에러: .env 파일에서 TMDB_API_KEY를 찾을 수 없습니다.")
+            return pd.DataFrame()
+
+        for page in range(1, max_pages + 1):
+            url = f"https://api.themoviedb.org/3/movie/popular?api_key={self.api_key}&language=ko-KR&page={page}"
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.json()
+                movies = data.get("results", [])
+                all_movies.extend(movies)
+
+                if page % 5 == 0:
+                    print(f"{page} 페이지까지 데이터 수집 완료.")
+
+            except requests.RequestException as e:
+                print(f"요청 실패: {page} --> {e}")
+                break
+
+        print(f"데이터 수집 완료 : 총 {len(all_movies)}개의 영화 정보.")
+        return pd.DataFrame(all_movies)
